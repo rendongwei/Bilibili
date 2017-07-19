747c2714a1d263f8a6de2a4ab0320341a3d0c842
rendongwei <19900122_rdw@163.com>
2017-07-19 16:34:54 +0800

    修改

--- app/src/main/java/com/don/bilibili/fragment/HomeFragment.java
+++ app/src/main/java/com/don/bilibili/fragment/HomeFragment.java
@@ -8,11 +8,15 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.don.bilibili.R;
+import com.don.bilibili.activity.HomeActivity;
 import com.don.bilibili.annotation.Id;
 import com.don.bilibili.annotation.OnClick;
 import com.don.bilibili.fragment.base.BindFragment;
+import com.don.bilibili.utils.DisplayUtil;
 
-public class HomeFragment extends BindFragment implements View.OnClickListener{
+import java.lang.reflect.Field;
+
+public class HomeFragment extends BindFragment implements View.OnClickListener {
 
     @Id(id = R.id.home_layout_top_menu)
     @OnClick
@@ -57,7 +61,22 @@ public class HomeFragment extends BindFragment implements View.OnClickListener{
 
     @Override
     protected void bindListener() {
+        mVpDisplay.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
 
+            @Override
+            public void onPageSelected(int arg0) {
+                mLayoutRecommendHead.setVisibility(arg0 == 1 ? View.VISIBLE
+                        : View.GONE);
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+            }
+        });
     }
 
     @Override
@@ -67,6 +86,54 @@ public class HomeFragment extends BindFragment implements View.OnClickListener{
 
     @Override
     public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.home_layout_top_menu:
+                ((HomeActivity) mActivity).showMenu();
+                break;
 
+            case R.id.home_layout_recommend_head:
+
+                break;
+
+            case R.id.home_layout_recommend_head_rank:
+
+                break;
+
+            case R.id.home_layout_recommend_head_attention:
+
+                break;
+        }
+    }
+
+    private void setIndicator() {
+        Class<?> tabLayout = mLayoutTab.getClass();
+        Field tabStrip = null;
+        try {
+            tabStrip = tabLayout.getDeclaredField("mTabStrip");
+        } catch (NoSuchFieldException e) {
+            return;
+        }
+
+        tabStrip.setAccessible(true);
+        LinearLayout ll_tab = null;
+        try {
+            ll_tab = (LinearLayout) tabStrip.get(mLayoutTab);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        int left = DisplayUtil.dip2px(mContext, 15);
+        int right = DisplayUtil.dip2px(mContext, 15);
+
+        for (int i = 0; i < ll_tab.getChildCount(); i++) {
+            View child = ll_tab.getChildAt(i);
+            child.setPadding(0, 0, 0, 0);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0,
+                    LinearLayout.LayoutParams.MATCH_PARENT, 1);
+            params.leftMargin = left;
+            params.rightMargin = right;
+            child.setLayoutParams(params);
+            child.invalidate();
+        }
     }
 }
