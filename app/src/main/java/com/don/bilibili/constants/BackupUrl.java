a703640a6a1c2d448f0688007aab9143db44b451
rendongwei <19900122_rdw@163.com>
2017-09-25 13:54:38 +0800

    修改默认播放地址

--- app/src/main/java/com/don/bilibili/constants/BackupUrl.java
+++ app/src/main/java/com/don/bilibili/constants/BackupUrl.java
@@ -7,5 +7,5 @@ public class BackupUrl {
     public static final String CCTV5plus_HD = "http://ivi.bupt.edu.cn/hls/cctv5phd.m3u8";
     public static final String CCTV6_HD = "http://ivi.bupt.edu.cn/hls/cctv6hd.m3u8";
 
-    public static final String ZHANQI = "http://dlhls.cdn.zhanqi.tv/zqlive/2869_tUPFz.m3u8";
+    public static final String ZHANQI = "http://dlhls.cdn.zhanqi.tv/zqlive/35180_KUDhx.m3u8";
 }
